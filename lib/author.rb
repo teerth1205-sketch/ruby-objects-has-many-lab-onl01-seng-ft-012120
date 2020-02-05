7cf7267a4f5c35dfa251e7a7ea26f44ad694fb49
Teerth Patel <teerth1205@gmail.com>
2020-02-05 01:09:33 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,1 +1,33 @@
-cla
+class Author
+  attr_accessor :name, :post
+  @@songcount = 0
+  def initialize(name)
+    @name  = name 
+    @s = []
+    
+  end
+  
+  def add_post(song)
+    @songs << song
+    song.artist = self
+    @@songcount += 1
+  end 
+  
+  def songs
+    Song.all
+  end 
+  
+  
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@songcount += 1
+  end 
+  
+  def self.song_count
+    Song.all.count
+  end 
+    
+  
+end 
