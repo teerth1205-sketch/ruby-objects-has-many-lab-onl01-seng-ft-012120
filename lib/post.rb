4946036ec50b10141885c0d30ac14009c1773a18
Teerth Patel <teerth1205@gmail.com>
2020-02-05 01:16:36 +0000

    Automatically backed up by Learn

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+class Song 
+  attr_accessor :title, :author
+  @@all = []
+  def initialize(title)
+    @title = title 
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
+  end 
+  
+  def author_name
+    if self.author == nil
+      nil
+    else 
+   self.author
+    end 
+    
+  end 
